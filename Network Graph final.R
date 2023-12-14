e4e018cbf41db64509aea8d2aeb93c9757738d3b
sale4cast <120444247+sale4cast@users.noreply.github.com>
2023-12-14 17:42:31 +0600

    Update Network Graph final.R
    In this code I change target node color, and use angle_calc = "along",   alpha = 1/5 . Now we not need vjust and hjust.

--- Network Graph final.R
+++ Network Graph final.R
@@ -21,6 +21,9 @@ server <- function(input, output) {
   data <- read.csv("E:/WORK/HOTEL DATA/4-starhotelinRiga.csv")
   data1 <- read.csv("E:/WORK//HOTEL DATA/4-starhotelinVienna.csv")
   data2 <- read.csv("E:/WORK/HOTEL DATA/4-starhotelinAmsterdam.csv")
+  
+  head=10
+  
   #for Riga 
   # hotelInfo <- tibble(
   #   hotelName = data %>% filter(RoomType == "Single") %>% pull(HotelName) %>% head(10),
@@ -47,15 +50,16 @@ server <- function(input, output) {
   # targetHotel <- "Hilton Vienna Waterfront"
   
   
+  
   #for Amsterdam
   hotelInfo <- tibble(
-    hotelName = data2 %>% filter(RoomType == "Single") %>% pull(HotelName) %>% head(10),
-    Rating = paste0("r- ",data2 %>% filter(RoomType == "Single") %>% pull(Ratings) %>% head(10)),
+    hotelName = data2 %>% filter(RoomType == "Single") %>% pull(HotelName) %>% head(head),
+    Rating = paste0("r- ",data2 %>% filter(RoomType == "Single") %>% pull(Ratings) %>% head(head)),
     
     
-    priceSingleRoom = paste0("৳ ",parse_number(data2 %>% filter(RoomType == "Single") %>% pull(Price) %>% head(10))),
-    priceDoubleRoom = paste0("৳ ",parse_number(data2 %>% filter(RoomType == "Double") %>% pull(Price) %>% head(10))),
-    priceTripleRoom = paste0("৳ ",parse_number(data2 %>% filter(RoomType == "Triple") %>% pull(Price) %>% head(10))),
+    priceSingleRoom = paste0("৳ ",parse_number(data2 %>% filter(RoomType == "Single") %>% pull(Price) %>% head(head))),
+    priceDoubleRoom = paste0("৳ ",parse_number(data2 %>% filter(RoomType == "Double") %>% pull(Price) %>% head(head))),
+    priceTripleRoom = paste0("৳ ",parse_number(data2 %>% filter(RoomType == "Triple") %>% pull(Price) %>% head(head))),
   )
   # #browser()
   targetHotel <- "DoubleTree by Hilton Amsterdam Centraal Station"
@@ -73,45 +77,12 @@ server <- function(input, output) {
     edges <- tibble(
       from = rep(rowIndexOfTargetHotel, length(hotelInfo$hotelName) - 1),
       to = setdiff(1:length(hotelInfo$hotelName), rowIndexOfTargetHotel),
-      #The to column is created by taking the set difference between the sequence of indices from 1 to the length of 
-      #hotelName and the index of the target hotel. This ensures that the target hotel is not connected to itself.
       rating = edgeRatings
     )
     
     graph <- tbl_graph(nodes = nodes, edges = edges)
-    #tbl_graph is a function from the tidygraph package that constructs a graph from node and edge data.
     E(graph)$edge_size <- (str_remove(edgeRatings , "^r-"))
     layout <- layout_with_kk(graph, weights = E(graph)$edge_size)
-    #E(graph) is used to access the edge data of the graph.
-    #$edge_size is used to extract the values in the edge_size column of the edge data.
-    #It stands for the Kamada-Kawai layout algorithm. This algorithm is used for arranging the nodes of a graph in a 
-    #visually pleasing way, taking into account the distances between nodes.
-    
-    if (length(hotelInfo$hotelName) <= 3) {
-      vjust = 1
-      hjust = -0.7
-    } else if (length(hotelInfo$hotelName) == 4) {
-      vjust = -1.3
-      hjust = -0.5
-    } else if (length(hotelInfo$hotelName) == 5) {
-      vjust = -0.4
-      hjust = -0.9
-    } else if (length(hotelInfo$hotelName) == 6) {
-      vjust = -1.3
-      hjust = 0.8
-    } else if (length(hotelInfo$hotelName) == 7) {
-      vjust = 1.5
-      hjust = 1.5
-    } else if (length(hotelInfo$hotelName) == 8) {
-      vjust = 1.9
-      hjust = 1.2
-    } else if (length(hotelInfo$hotelName) == 9) {
-      vjust = 1.8
-      hjust = 0.9
-    } else if (length(hotelInfo$hotelName) == 10) {
-      vjust = 0.5
-      hjust = -1.5
-    }
     
     TargetnodeSizefor2=25
     Neibornodesizefor2=23
@@ -121,13 +92,18 @@ server <- function(input, output) {
     hjustnode=0.5
     
     ggraph(graph, layout = layout) +
-      geom_edge_link(aes(label = rating), label_colour = "gold", vjust = vjust, hjust = hjust,
+      geom_edge_link(aes(label = rating), label_colour = "darkorange",
+                     angle_calc = "along", # angle_calc is set to "along," it means that the edges will be drawn along the calculated angle based on the layout of the graph. 
+                     alpha = 1/5,
                      show.legend = FALSE,
                      start_cap = circle(5, "mm"),
                      end_cap = circle(5, "mm"),
-                     color = "gold" ) +
-      geom_node_point(aes(x = layout[, 1], y = layout[, 2], color = as.factor(id)), 
+                     color = "grey15", width=1) +
+      geom_node_point(aes(x = layout[, 1], y = layout[, 2], color = as.factor(nodes$id)), 
                       size = ifelse(nodes$id == rowIndexOfTargetHotel, ifelse(length(hotelInfo$hotelName) == 2, TargetnodeSizefor2,  TargetnodeSizeforALL), ifelse(length(hotelInfo$hotelName) == 2,  Neibornodesizefor2,  NeibornodesizeforALL ))) +
+      geom_node_point(aes(x = layout[rowIndexOfTargetHotel, 1], y = layout[rowIndexOfTargetHotel, 2]), 
+                      color = "gray60", size = ifelse(length(hotelInfo$hotelName) == 2, TargetnodeSizefor2,  TargetnodeSizeforALL), show.legend = FALSE) +
+      
       geom_text(aes(x = layout[, 1], y = layout[, 2], label = str_wrap(nodes$label, width = 13)), 
                 color="black",
                 fontface=ifelse(nodes$id == rowIndexOfTargetHotel,"bold", "plain"),
@@ -139,12 +115,13 @@ server <- function(input, output) {
                label = hotelInfo$Rating[rowIndexOfTargetHotel],vjust = 4.6, hjust = hjustnode, size = ifelse(length(hotelInfo$hotelName) == 2, 2.8, 3.5), color = "darkblue", fontface="bold") +
       theme_void() +
       theme(
-        panel.background = element_rect(fill = "black"),  # Set the panel background color
-        plot.background = element_rect(fill = "black"),   # Set the plot background color
+        panel.background = element_rect(fill = "black"),
+        plot.background = element_rect(fill = "black"),
         plot.margin = margin(0.1, 0.1, 0.1, 0.1, "cm"),
         legend.position = "none"
       )
   }
+  
   
   output$plot <- renderUI({
     lapply(3:length(hotelInfo), function(rowIndex){  
